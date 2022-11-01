749ce8375424fb4d851874058617f744f907cf6a
Thkt18 <110399102+Thkt18@users.noreply.github.com>
2022-11-01 17:18:25 +0100

    Config script + product

---
+++ front/js/script.js
@@ -0,0 +1,62 @@
+fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => addProducts(data))
+
+    function addProducts(donnees) {
+
+        donnees.forEach((canapé) => {
+            console.log("canapé:", canapé)
+            
+            const  {id, imageUrl, altTxt, name, description} = canapé
+            const anchor = makeAnchor(id)
+            const article = document.createElement("article")
+            const image = makeImage(imageUrl, altTxt)
+            const h3 = makeH3(name)
+            const p = makeParagraph(description)
+
+            appendElementsToArticle(article, image, h3, p)
+            appendChildren(anchor, article)
+        })
+    }
+
+    function appendElementsToArticle(article, image, h3, p) {
+        article.appendChild(image)
+        article.appendChild(h3)
+        article.appendChild(p)
+    }
+
+    function makeAnchor(id) {
+        const anchor = document.createElement("a")
+        anchor.href = "./product.html?id=" + id
+        return anchor
+    }
+
+    function appendChildren(anchor, article) {
+        const items = document.querySelector("#items")
+        if (items != null) {
+            items.appendChild(anchor)
+            anchor.appendChild(article)
+        }
+    }
+
+function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img")
+    image.src = imageUrl
+    image.alt = altTxt
+    return image
+}
+
+
+function makeH3(name) {
+    const h3 = document.createElement("h3")
+    h3.textContent = name
+    h3.classList.add("productName")
+    return h3
+}
+
+function makeParagraph(description) {
+    const p = document.createElement("p")
+    p.textContent = description
+    p.classList.add("productDescription")
+    return p
+ }
